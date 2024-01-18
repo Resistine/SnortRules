9564a7777c235f06d098fc9ff67c0438107cf884
Petr Pospíšil <30226779+SleepyLctl@users.noreply.github.com>
2024-01-18 17:49:49 +0100

    Parser fixed, now it parses all the main fields.

--- run_parse_snort_rules.py
+++ run_parse_snort_rules.py
@@ -3,17 +3,20 @@ from snortparser import Parser
 with open('csv_rules_parsed.csv', 'a') as csv_rules:
     with open("snort3-community.rules") as snort_rules:
         for rule in snort_rules:
-            parsed = Parser(rule)
-            
-            #print("Type of data after going thru parser lib", type(parsed),"\n")
-            
+            parsed = Parser(rule)            
             try:
-                parsed_string = parsed.header['action'] + ';'  + parsed.header['proto'] + ';' + str(parsed.header['source']) + ';' +  str(parsed.header['src_port']) + ';' + parsed.header['arrow'] + ';' +  str(parsed.header['destination']) + ';' +  str(parsed.header['dst_port']) + ';' +  '--------' + ';' +  str(parsed.options[0][1]) + ';' +  str(parsed.options[2]) +  str(parsed.options[3]) +  str(parsed.options[4]) +  str(parsed.options[5]) +  str(parsed.options[6]) +  str(parsed.options[7]) +  str(parsed.options[8]) + '\n'
-                print('pass')
+                parsed_string = parsed.header['action'] + ';'  + parsed.header['proto'] + ';' + str(parsed.header['source']) + ';' +  str(parsed.header['src_port']) + ';' + parsed.header['arrow'] + ';' +  str(parsed.header['destination']) + ';' +  str(parsed.header['dst_port']) + ';' +  '--------' + ';' +  str(parsed.options[0][1]) + ';'
+                
+                counter = 1
+                while True:
+                    parsed_string += str(parsed.options[counter]) + ';'
+                    counter += 1 
+                
             except:
-                parsed_string = parsed.header['action'] + ';'  + parsed.header['proto'] + ';' + str(parsed.header['source']) + ';' +  str(parsed.header['src_port']) + ';' + parsed.header['arrow'] + ';' +  str(parsed.header['destination']) + ';' +  str(parsed.header['dst_port']) + ';' +  '--------' + ';' +  str(parsed.options[0][1]) + '\n'
-            
-            print(parsed_string)
-            csv_rules.write(parsed_string)
-        
-print('Neplecha ukoncena...')
+                parsed_string += "\n"
+                print("I parsed " + str(counter) + " option fields.\n\n")
+                print(parsed_string)
+                csv_rules.write(parsed_string)
+            parsed_string = ""            
+
+print('Job has been done; It\'s mess, but done...')
